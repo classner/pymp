9895ec2ec01ad2778a400449cbfa17f162491180
Christoph Lassner <classner@amazon.com>
2018-09-07 23:01:00 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 The setup script for the entire project.
 @author: Christoph Lassner
 """
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-VERSION = '0.4.0'
+VERSION = "0.4.2"
 
 setup(
-    name='pymp-pypi',
-    author='Christoph Lassner',
-    author_email='mail@christophlassner.de',
+    name="pymp-pypi",
+    author="Christoph Lassner",
+    author_email="mail@christophlassner.de",
     packages=find_packages(),
     version=VERSION,
-    test_suite='tests.unittests',
-    license='MIT License',
-    url='https://github.com/classner/pymp',
-    download_url='https://github.com/classner/pymp/tarball/v{0}'.format(VERSION),
-    keywords=['multiprocessing', 'openmp', 'parallelism']
+    test_suite="tests.unittests",
+    license="MIT License",
+    url="https://github.com/classner/pymp",
+    download_url="https://github.com/classner/pymp/tarball/v{0}".format(VERSION),
+    keywords=["multiprocessing", "openmp", "parallelism"],
 )
